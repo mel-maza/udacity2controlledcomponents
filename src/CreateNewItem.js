c98e6f645b4190c9c4488d08b598cf371451d6a3
Your Name <you@example.com>
2018-09-27 05:22:39 +0000

    code complete and now working

--- src/CreateNewItem.js
+++ src/CreateNewItem.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 
 class CreateNewItem extends Component {
+  	state = {
+    	itemValue: ''
+    }
+
+	handleChange = event => {
+    this.setState({ itemValue: event.target.value });
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    this.props.onAddItem(this.state.itemValue);
+  };
+
+  inputIsEmpty = () => {
+    return this.state.itemValue === '';
+  };
 	render() {
     	return (
         	<form onSubmit={this.addItem}>
-<input
-type="text"
-placeholder="Enter New Item"
-value={this.state.value}
-onChange={this.handleChange}
-/>
-<button disabled={this.inputIsEmpty()}>Add</button>
-</form>
+              <input
+              type="text"
+              placeholder="Enter New Item"
+              value={this.state.itemValue}
+              onChange={this.handleChange}
+              />
+              <button disabled={this.inputIsEmpty()}>Add</button>
+              </form>
         )
     }
 }
